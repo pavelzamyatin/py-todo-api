bb11c960f779236bf2bfde01f249d5abc04a7367
Corw <pzamyatin@gmail.com>
2016-07-05 14:07:00 +1200

    Added GET requests

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+app.url_map.strict_slashes = False
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+# @app.route('/')
+# def index():
+#     return "Hello, World!"
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    for task in tasks:
+        if task['id'] == task_id:
+            return jsonify({'task' : task})
+
+    abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
